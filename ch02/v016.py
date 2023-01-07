70c32fb6a212b3dd273d5f66fa6756ea26f835ae
Alexis <cucoalexis@hotmail.com>
2023-01-07 22:00:57 +0100

    A full Python refresher

---
+++ ch02/v016.py
@@ -0,0 +1,10 @@
+friends = ["Rolf", "Bob", "Jen"]
+
+print("Jen" in friends)
+
+movies_watched = {"The Matrix", "Green Book", "Her"}
+print("Her" in movies_watched)
+print("Him" in movies_watched)
+
+user_movie = input("Enter something you've watched recently: ")
+print(user_movie in movies_watched)
